2c549a65450c6a7389226219e4fe996c93c38b8a
KanedaShotaro <javier@javiercarrillo.es>
2014-07-11 14:42:20 +0200

    Javascript para Etiquetas, El boton Examniar de Crop no funciona en "registroRecetaView"

--- Model/poolConexion.php
+++ Model/poolConexion.php
@@ -2,9 +2,9 @@
 
 function poolBBDD() {
     /* Servidor Javi*/
-    // $bd = new BaseDatos('192.168.56.3', 'alumno', 'alumno', 'SALPIMENTA');
+     $bd = new BaseDatos('192.168.56.3', 'alumno', 'alumno', 'SALPIMENTA');
     /*Servidor Pablo*/
-    $bd = new BaseDatos('127.0.0.1', 'root', 'root', 'salpimenta');
+//    $bd = new BaseDatos('127.0.0.1', 'root', 'root', 'salpimenta');
     return $bd;
 }
 
--- View/layoutView.php
+++ View/layoutView.php
@@ -5,7 +5,7 @@ if (isset($_GET["zona"])) {
         $misalpimenta = "activo-list-option";
     } else if ($_GET["zona"] == "explora") {
         $explora = "activo-list-option";
-    }else{
+    } else {
         $blogs = "activo-list-option";
     }
 } else {
@@ -18,23 +18,26 @@ if (isset($_GET["zona"])) {
     <head>
         <meta charset="UTF-8">
         <title></title>
+
+        <link type="text/css" media="screen" rel="stylesheet" href="/View/css/croppic.css"> <!-- css recorte -->
         <link rel="stylesheet" href="/View/css/rateit.css">
         <link rel="stylesheet" href="/View/css/alerts.css"> 
-        <!-- <link rel="stylesheet" href="/View/css/offcanvas.css"> -->
         <link rel="stylesheet" href="/View/css/main.css"/>
         <link rel="stylesheet" href="/View/css/icomoon.css"/>
-       
+
+
+        <!-- Test recortes imagen -->
+        <script type="text/javascript" src="/View/js/croppic.js"></script>
+        <script type="text/javascript" src="/View/js/main.js"></script> 
 
         <script src="/View/js/jquery.rateit.js"></script>
         <script src="/View/js/jquery-2.1.0.min.js"></script>
         <script src="/View/js/bootstrap.js"></script>
         <script src="/View/js/jquery.rateit.js"></script>
         <script src="/View/js/jquery-ui.js"></script>
-        
-        <!-- Test recortes imagen -->
-        <link type="text/css" media="screen" rel="stylesheet" href="/View/css/croppic.css">
-        <script type="text/javascript" src="/View/js/croppic.js"></script>
-        <script type="text/javascript" src="/View/js/main.js"></script>
+        <script src="/View/js/jquery.tagsinput.js"></script>
+
+
 
     </head>
     <body>
