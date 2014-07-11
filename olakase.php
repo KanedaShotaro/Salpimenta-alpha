3300b627c3dd62a7a79b50bd2b0828146c8ab8b4
KanedaShotaro <javier@javiercarrillo.es>
2014-07-11 19:25:51 +0200

    Empiezo el branch para implementar comentarios a las recetas

--- olakase.php
+++ olakase.php
@@ -2,6 +2,7 @@
 echo 'ola k ase';
 echo  'probando o ke ase';
 echo 'me cuenta un commit o k ase';
+echo "hola";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
