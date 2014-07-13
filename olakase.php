f2262f88f373b486d7feb9c1ef606c8f70e85eab
Pablo Carrillo <pablo@pablocarrillo.net>
2014-07-13 21:52:29 +0200

    Actualizacion de Master casa mama branch krleza

--- olakase.php
+++ olakase.php
@@ -4,6 +4,8 @@ echo  'probando o ke ase';
 echo 'me cuenta un commit o k ase';
 echo "hola";
 echo "termino el branch";
+echo "abro de nuevo el branch krleza";
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
